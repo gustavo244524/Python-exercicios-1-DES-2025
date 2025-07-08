511961448672be4184f31773f1ae29fd64944167
gustavo244524 <gustavo.pupo.pereira@escola.pr.gov.br>
2025-07-08 11:45:09 +0000

    feito

--- exercicio011.py
+++ exercicio011.py
@@ -1,9 +1,10 @@
+# Crie um programa que receba o peso (kg) e a altura (m) de uma pessoa e calcule o IMC.
+# Classifique o resultado de acordo com a faixa:
 
-
-
-
-
-
+# Abaixo do peso (< 18.5)
+# Peso normal (18.5 a 24.9)
+# Sobrepeso (25 a 29.9)
+# Obesidade (>= 30)
 
 
 peso = float(input("Digite o peso (kg): "))  
@@ -19,3 +20,4 @@ elif imc < 25:
     print("Peso normal") 
 else: 
     print("Acima do peso.")
+    
